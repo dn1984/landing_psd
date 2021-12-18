cb9cb1632caad9025777a99462f547f9e864932b
dn1984 <kils@list.ru>
2021-12-18 19:13:43 +0400

    Закончена основная верстка

---
+++ src/pages/main/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#mobile-menu-btn').click(function() {
+        $('#mobile-menu').toggleClass('mobile-menu-close_js');
+        $('#mobile-menu-wrapper').toggleClass('mobile-menu-close_js');
+    });
+
+    $('#mobile-menu__close-btn').click(function() {
+        $('#mobile-menu').toggleClass('mobile-menu-close_js');
+        $('#mobile-menu-wrapper').toggleClass('mobile-menu-close_js');
+    });
+});
